1c1d0132614067229c3e072f6f199d33135247a3
Pratik Das <pratik14901@gmail.com>
2022-07-05 00:38:20 +0530

    post part done 1

--- app.js
+++ app.js
@@ -3,7 +3,50 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const router = express.Router();
 
+const url = require('./secret');
+
 const app = express();
+
+app.use(bodyParser.json());    //return parameter as JSON
+
+const client = new MongoClient(url, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true           //creating client to connect to db
+});
+ 
+
+// MongoClient.connect(url, (err,db) => {
+//     if (err) throw err;
+//     console.log("Connected");
+
+//     db.close();
+// })
+
+client.connect(err => {
+    const myDB = client.db('People').collection('Friends');
+
+    app.route('/users')
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+        console.log(req.body);
+        myDB.insertOne(req.body).then(result => {
+            console.log(req.body);
+            res.contentType('application/json');
+            res.send(JSON.stringify(result));
+        })
+    })
+    .put((req, res) => {
+
+    })
+    .delete((req, res) => {
+  
+    })
+
+})
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
---
+++ secret.js
@@ -0,0 +1,3 @@
+const url = "mongodb+srv://nodePratik:mongo-node@cluster0.ug7gh1u.mongodb.net/?retryWrites=true&w=majority";
+
+module.exports = url;
